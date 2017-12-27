d58966fa5f1693e0e77b8543c7ca4ce8d439f6fd
Ryanne Dolan <ryannedolan@gmail.com>
2017-12-27 11:17:24 -0600

    futures

---
+++ samples/futures.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "github.com/ryannedolan/flim/fl"
+  "fmt"
+)
+
+func main () {
+  foobar := fl.Promise(fl.Success("foo")).Iter().Map(` x + "bar" `).List()
+  fmt.Println(foobar)
+
+  nobar := fl.Promise(fl.Failuref("foo")).OrElse(fl.Success("no")).Iter().Map(` x + "bar" `).List()
+  fmt.Println(nobar)
+
+  rpc := func() (string, error) {
+    return "apples", nil
+  }
+
+  f := fl.Future()
+  f.CompleteWith(func() (interface{}, error) { return rpc() })
+  resp, err := f.Wait()
+  fmt.Println("server says:", resp, err)
+}
