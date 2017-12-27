e7e0cd573e83f0d9e7dedd6c288c1891511831e4
Ryanne Dolan <ryannedolan@gmail.com>
2017-12-27 11:06:21 -0600

    some basic HOFs

---
+++ fl/hof.go
@@ -0,0 +1,15 @@
+package fl
+
+type fn func(...interface{}) interface{}
+
+func Compose(f1 fn, f2 fn) fn {
+  return func(args ...interface{}) interface{} {
+    return f2(f1(args...))
+  }
+}
+
+func Curry(f fn, args ...interface{}) fn {
+  return func(args2 ...interface{}) interface{} {
+    return f(append(args, args2...)...)
+  }
+}
---
+++ samples/hof.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "github.com/ryannedolan/flim/fl"
+  "fmt"
+)
+
+func main() {
+  f := fl.Lambda(`x+1`)
+  g := fl.Lambda(`x+2`)
+  h := fl.Lambda(`x+y`)
+
+  fog := fl.Compose(f,g)
+  fmt.Println("fog(1) = ", fog(1))
+
+  cur := fl.Curry(h, 1)
+  fmt.Println("h(1)(2) = ", cur(2))
+
+  cur2 := fl.Curry(h, 1, 2)
+  fmt.Println("h(1,2)() = ", cur2())
+
+  cur3 := fl.Curry(h)
+  fmt.Println("h()(1,2) = ", cur3(1,2))
+}
